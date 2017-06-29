79c20dd2bd4064ec808754db8c607a7612493ead
youngstone <youngstone@lgcns.com>
2017-06-29 14:13:01 +0900

    added post_draft_list

--- blog/urls.py
+++ blog/urls.py
@@ -12,5 +12,7 @@ urlpatterns= [
 	url(r'^$', views.post_list, name='post_list'),
 	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 	url(r'^post/new/$', views.post_new, name='post_new'),
-	url(r'^post/edit/(?P<pk>\d+)$',views.post_edit, name='post_edit'),
+	url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+	url(r'^post/drafts/$', views.post_draft_list,name='post_draft_list'),
+	url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -26,7 +26,7 @@ def post_new(request):
 			if form.is_valid():
 				post = form.save(commit=False)
 				post.author = request.user
-				post.published_date = timezone.now()
+				# post.published_date = timezone.now()
 				post.save()
 				return redirect('post_detail',pk=post.pk)
 	else:
@@ -36,15 +36,25 @@ def post_new(request):
 	return render(request, 'blog/post_edit.html',{'form':form})
 
 def post_edit(request, pk):
-	post = get_object_or_404(Post,pk)
+	post = get_object_or_404(Post,pk=pk)
 	if request.method == "POST":
 		form = PostForm(request.POST, instance=post)
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
+			# post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm(instance=post)
 	return render(request, 'blog/post_edit.html', {'form':form})
+
+def post_draft_list(request):
+	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+	return render(request, 'blog/post_draft_list.html',{'posts':posts})
+	
+def post_publish(request, pk):
+	post = get_object_or_404(Post,pk=pk)
+	post.publish()
+	return redirect('post_detail', pk=pk)
+
