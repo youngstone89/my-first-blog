f8077671f922ae2bd0e3625719d730cfd31cd7e0
youngstone <youngstone@lgcns.com>
2017-06-29 14:55:48 +0900

    added post_remove in views

--- blog/urls.py
+++ blog/urls.py
@@ -8,11 +8,11 @@ from . import views
 # $ - "마지막"! 입니다. 그 뒤로 더이상 문자가 오면 안됩니다.#
 
 urlpatterns= [
-	
 	url(r'^$', views.post_list, name='post_list'),
 	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 	url(r'^post/new/$', views.post_new, name='post_new'),
 	url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
-	url(r'^post/drafts/$', views.post_draft_list,name='post_draft_list'),
+	url(r'^post/drafts/$', views.post_draft_list, name='post_draft_list'),
 	url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+	url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
 ]
