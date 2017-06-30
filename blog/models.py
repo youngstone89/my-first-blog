763b5820d1c3604ccaa6db58a8ad12f92ae2ae1e
youngstone <youngstone@lgcns.com>
2017-06-30 16:42:12 +0900

    add comment

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 
+# Create your models here.
 
 class Post(models.Model):
 	author = models.ForeignKey('auth.User')
@@ -9,16 +10,32 @@ class Post(models.Model):
 	text = models.TextField()
 	created_date=models.DateTimeField(default=timezone.now)
 	published_date=models.DateTimeField(blank=True,null=True)
+	
+	def approved_comments(self):
+		return self.comments.filter(approved_comment=True)
 
 
 	def publish(self):
-		self.published_date=timezone.now()
+		self.published_date=timezone.now
 		self.save()
 
 	def __str__(self):
 		return self.title
-# Create your models here.
 
+class Comment(models.Model):
+	post = models.ForeignKey('blog.Post',related_name='comments')
+	author = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	approved_comment = models.BooleanField(default=False)
+
+
+	def approve(self):
+		self.approved_comment = True
+		self.save()
+
+	def __str__(self):
+		return self.text
 
 
 # class IPAddress(models.Model):
